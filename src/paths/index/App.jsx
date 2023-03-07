c07278cd4a01180cf5b4da09301780d01107c006
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2023-03-07 02:15:25 -0600

    fixed water order issue

--- src/paths/index/App.jsx
+++ src/paths/index/App.jsx
@@ -196,7 +196,7 @@ const App = () => {
           </Link>
         </section>
         <section className='amtrakerVersion'>
-          <p>Amtraker v3.2.1</p>
+          <p>Amtraker v3.2.2</p>
           <p>&copy; Piero Maddaleni 2023</p>
           {/*
           <p>
