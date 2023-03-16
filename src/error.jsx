376bb697613cb3e907f3b91242d9f77051ca592f
Piero Maddaleni <22846928+piemadd@users.noreply.github.com>
2023-03-16 10:29:16 -0500

    changed up error message

--- src/error.jsx
+++ src/error.jsx
@@ -3,6 +3,8 @@ import { useRouteError } from "react-router-dom";
 export default function ErrorPage() {
   const error = useRouteError();
 
+  const errorString = error.toString();
+
   return (
     <div id='error-page'>
       <h1>Oops!</h1>
@@ -13,29 +15,42 @@ export default function ErrorPage() {
         inconvenience.
       </p>
       <br />
+      {errorString.includes("error loading dynamically imported module") ? (
+        <p>
+          Seems like the issue you're facing might be data related, which is
+          usually intermittent. Please try again in 3-4 minutes. I aplogize for
+          the inconvenience
+        </p>
+      ) : null}
       <p>
         <i>
           Current path: {window.location.href}
+          <br />
+          Current version: 3.4.3
           <br />
           Current date and time (UTC): {new Date().toUTCString()}
           <br />
           Current date and time (local): {new Date().toLocaleString()}
           <br />
           {error.toString()}
-          <br />
+          {/*
+            <br />
           Error Trace:
+      */}
         </i>
       </p>
-      <div>
-        {error.stack
-          .split("\n")
-          .filter((n) => n)
-          .map((line, i) => (
-            <p className='errorTrace' key={`error-trace-${i}`}>
-              {line}
-            </p>
-          ))}
-      </div>
+      {/*
+        <div>
+          {error.stack
+            .split("\n")
+            .filter((n) => n)
+            .map((line, i) => (
+              <p className='errorTrace' key={`error-trace-${i}`}>
+                {line}
+              </p>
+            ))}
+        </div>
+            */}
     </div>
   );
 }
