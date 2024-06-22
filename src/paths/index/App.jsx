64822b9db0c4b0f729154c46fa5708f5e120dd29
Piero Maddaleni <piero@piemadd.com>
2024-06-22 17:54:30 -0500

    Added oembed discovery headers

--- src/paths/index/App.jsx
+++ src/paths/index/App.jsx
@@ -237,7 +237,7 @@ const App = () => {
           </Link>
         </section>
         <section className='amtrakerVersion'>
-          <p>Amtraker v3.10.8</p>
+          <p>Amtraker v3.10.9</p>
           <p>&copy; Piero Maddaleni 2024</p>
         </section>
         <SenseBlock key={"sense-block"} dataAdSlot={"3140178047"} />
