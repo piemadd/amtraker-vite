fed1d2b970c18d391ff48495559d7b97cde4c900
Piero Maddaleni <piero@piemadd.com>
2024-08-19 19:35:57 -0500

    no more gay title + fixed version num

--- src/paths/index/App.jsx
+++ src/paths/index/App.jsx
@@ -237,7 +237,7 @@ const App = () => {
           </Link>
         </section>
         <section className='amtrakerVersion'>
-          <p>Amtraker v3.11.0</p>
+          <p>Amtraker v3.11.2</p>
           <p>&copy; Piero Maddaleni 2024</p>
         </section>
         <SenseBlock key={"sense-block"} dataAdSlot={"3140178047"} />
