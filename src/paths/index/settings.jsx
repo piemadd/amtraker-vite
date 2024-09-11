39f3890a0c368d5de297ca0ab5761c34f09eab0c
Piero Maddaleni <piero@piemadd.com>
2024-09-11 10:10:44 -0500

    Update settings.jsx

--- src/paths/index/settings.jsx
+++ src/paths/index/settings.jsx
@@ -67,64 +67,7 @@ const Settings = () => {
               Light
             </option>
           </select>
-          <h2>Foamer Mode</h2>
-          <p>
-            Foamer Mode makes using Amtraker a bit more pleasant for foamers,
-            but its features may be confusing and simply not useful for regular
-            users. If you are wondering what a foamer is, you probably aren't
-            one, but "foamer" is a term used to describe rail fans: people who
-            take pictures of trains for fun. Here is a list of the current
-            features:
-          </p>
-          <ul>
-            <li>
-              <i>Train ETA:</i> Using a user's location, Amtraker can estimate
-              when a train will pass a user along the route. The algorithm is
-              far from perfect (it tends to underestimate the ETA), but it can
-              be helpful for those who want to time their shots better. This
-              feature is currently in beta and should improve over time.
-            </li>
-            <li>
-              <i>User Location:</i> The location of a user is shown on the
-              tracking map. This can be helpful for those who are new to a
-              location and aren't sure where they are in relation to the train
-              route.
-            </li>
-          </ul>
-          <select
-            onChange={(e) => {
-              console.log("changing foamer mode to", e.target.value);
-              let settings = JSON.parse(
-                localStorage.getItem("amtraker-v3-settings")
-              );
-              settings.foamerMode = e.target.value === "true" ? true : false;
-              localStorage.setItem(
-                "amtraker-v3-settings",
-                JSON.stringify(settings)
-              );
-            }}
-          >
-            {JSON.parse(localStorage.getItem("amtraker-v3-settings"))
-              .foamerMode ? (
-              <>
-                <option value='true' key='foamer-mode-enabled'>
-                  Enabled
-                </option>
-                <option value='false' key='foamer-mode-disabled'>
-                  Disabled
-                </option>
-              </>
-            ) : (
-              <>
-                <option value='false' key='foamer-mode-disabled'>
-                  Disabled
-                </option>
-                <option value='true' key='foamer-mode-enabled'>
-                  Enabled
-                </option>
-              </>
-            )}
-          </select>
+          
           <h2>More?</h2>
           <p>
             If you have any feature requests for Amtraker, please send them my
