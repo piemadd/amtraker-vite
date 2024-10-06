be986d0742e7b33b700433232f593b3f6740c7da
Piero Maddaleni <piero@piemadd.com>
2024-10-06 14:10:40 -0500

    updated packages

--- src/paths/index/App.jsx
+++ src/paths/index/App.jsx
@@ -237,7 +237,7 @@ const App = () => {
           </Link>
         </section>
         <section className='amtrakerVersion'>
-          <p>Amtraker v3.12.0</p>
+          <p>Amtraker v3.12.1</p>
           <p>&copy; Piero Maddaleni 2024</p>
         </section>
         <SenseBlock key={"sense-block"} dataAdSlot={"3140178047"} />
