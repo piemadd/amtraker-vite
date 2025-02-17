3bcc4fd9990467b2794c0f38262360b62dc02afd
piemadd <piero@piemadd.com>
2025-02-17 01:38:27 -0600

    updated copyright year + sitemap, build 7

--- src/error.jsx
+++ src/error.jsx
@@ -59,7 +59,7 @@ export default function ErrorPage() {
           <i>
             Current path: {window.location.href}
             <br />
-            Current version: Beta 3.13.0 Build 6
+            Current version: Beta 3.13.0 Build 7
             <br />
             Current date and time (UTC): {new Date().toUTCString()}
             <br />
@@ -85,7 +85,7 @@ export default function ErrorPage() {
           <i>
             Current path: {window.location.href}
             <br />
-            Current version: Beta 3.13.0 Build 6
+            Current version: Beta 3.13.0 Build 7
             <br />
             Current date and time (UTC): {new Date().toUTCString()}
             <br />
