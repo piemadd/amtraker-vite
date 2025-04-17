4c8be5265465307a8d09ab5349455c8c136399f9
piemadd <piero@piemadd.com>
2025-04-16 22:22:14 -0500

    oops, swapped

--- functions/trains/_middleware.js
+++ functions/trains/_middleware.js
@@ -18,37 +18,39 @@ export async function onRequest(context) {
   const res = await next()
   const { pathname } = new URL(request.url)
 
-  if (!pathname.startsWith('/stations')) return res; // not entirely sure how we got here tbh
-  if (pathname.endsWith('stations')) return res; // no station specified
+  if (!pathname.startsWith('/trains')) return res; // not entirely sure how we got here tbh
+  if (pathname.endsWith('trains')) return res; // no station specified
 
-  let code = pathname.split('/').at(-1);
+  let code = pathname.replace('/trains/', '').replaceAll('/', '-');
   let ogtag
 
-  const infoRes = await fetch(`http://api.amtraker.com/v3/stations/${code}`);
+  const infoRes = await fetch(`http://api.amtraker.com/v3/trains/${code}`);
   const infoDataRaw = await infoRes.text();
 
-  if (infoDataRaw == 'Not founds') return res; // doesnt exist
+  if (infoDataRaw == 'Not found') return res; // doesnt exist
 
-  const infoData = JSON.parse(infoDataRaw);
+  let infoData = JSON.parse(infoDataRaw);
 
   if (Array.isArray(infoData) && infoData.length == 0) return res; // doesnt exist
 
+  infoData = infoData[code.split('-')[0]][0];
+
   // these are the metatags we want to inject into the site
   ogtag = `
-    <meta property="og:title" content="${infoData[code].name} (${code}) | Amtraker" />
-    <meta property="og:description" content="Track trains to and from ${infoData[code].name}!" />
+    <meta property="og:title" content="${infoData.routeName} (${infoData.trainNum}) | Amtraker" />
+    <meta property="og:description" content="Track the ${infoData.routeName} on Amtraker!" />
     <meta property="og:locale" content="en_US" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="${request.url}" />
-    <meta property="og:image" content="https://ogimg.transitstat.us/images?service=amtraker&type=station&code=${code}" />
+    <meta property="og:image" content="https://ogimg.transitstat.us/images?service=amtraker&type=train&code=${code}" />
 
     <meta property="og:image:height" content="630" />
     <meta property="og:image:width" content="1200" />
 
     <meta name="twitter:card" content="summary_large_image">
     <meta property="twitter:url" content="${request.url}">
-    <meta name="twitter:title" content="${infoData[code].name} (${code}) | Amtraker">
-    <meta name="twitter:description" content="Track trains to and from ${infoData[code].name}!">
+    <meta name="twitter:title" content="${infoData.routeName} (${infoData.trainNum}) | Amtraker">
+    <meta name="twitter:description" content="Track the ${infoData.routeName} on Amtraker!">
     <meta name="twitter:image" content="https://ogimg.transitstat.us/images?service=amtraker&type=station&code=${code}">
   `
 
