9d4b5f1bdfe99ecfeeee28500996d5321acbc320
Piero Maddaleni <piero@piemadd.com>
2026-02-12 20:57:27 -0600

    Patch 8

--- src/paths/index/App.jsx
+++ src/paths/index/App.jsx
@@ -270,7 +270,7 @@ const App = () => {
           </Link>
         </section>
         <section className='amtrakerVersion section-border'>
-          <p>Amtraker v3.17.4 P7</p>
+          <p>Amtraker v3.17.4 P8</p>
           <p>&copy; <a href="https://piemadd.com" target="_blank">Piero Maddaleni</a> 2026</p>
         </section>
       </main>
