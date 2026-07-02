759e33f444a138818d25ed948fafdb409e10abfd
Piero Maddaleni <piero@piemadd.com>
2026-07-02 10:59:48 -0500

    Oops!

--- src/paths/index/App.jsx
+++ src/paths/index/App.jsx
@@ -298,7 +298,7 @@ const App = () => {
         </div>
 
         <section className="footer section-border">
-          <p>Amtraker v3.19.11</p>
+          <p>Amtraker v3.19.12</p>
           <div>
             <Link to="/about">
               <p>About/Settings/Privacy</p>
