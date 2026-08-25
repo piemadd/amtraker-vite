9f3c511b34291bcd6ef4c4875562151f2d4bd8a2
Piero Maddaleni <piero@piemadd.com>
2026-08-25 04:43:10 -0500

    Force update to clear cache

--- functions/atlas/_middleware.js
+++ functions/atlas/_middleware.js
@@ -25,8 +25,8 @@ export async function onRequest(context) {
 
   const infoRes = await fetch(`http://api.amtraker.com/v3/trains/${code}`, {
     headers: {
-      'User-Agent': "AmtrakerViteWorker/v3.21.6 (+https://amtraker.com)",
-      'Amtraker-Version': "v3.21.6",
+      'User-Agent': "AmtrakerViteWorker/v3.21.7 (+https://amtraker.com)",
+      'Amtraker-Version': "v3.21.7",
     }
   });
   const infoDataRaw = await infoRes.text();
--- src/components/dataManager/dataManager.js
+++ src/components/dataManager/dataManager.js
@@ -26,7 +26,7 @@ export class DataManager {
         console.log("DM-R:", this._id);
         fetch(URL_TO_USE, {
           cache: "reload",
-          headers: { "User-Agent": "AmtrakerVite/v3.21.6 (+https://amtraker.com)", "Amtraker-Version": "v3.21.6" }
+          headers: { "User-Agent": "AmtrakerVite/v3.21.7 (+https://amtraker.com)", "Amtraker-Version": "v3.21.7" }
         })
           .then((res) => res.json())
           .then((data) => {
