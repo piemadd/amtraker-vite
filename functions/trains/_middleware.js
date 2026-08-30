26b7c59c87d4953e2a0058b63816269d4119d31a
Piero Maddaleni <piero@piemadd.com>
2026-08-30 17:15:04 -0500

    Increased text size of Gulf of Mexico and Lake Ontario

--- functions/trains/_middleware.js
+++ functions/trains/_middleware.js
@@ -26,8 +26,8 @@ export async function onRequest(context) {
 
   const infoRes = await fetch(`http://api.amtraker.com/v3/trains/${code}`, {
     headers: {
-      'User-Agent': "AmtrakerViteWorker/v3.21.11 (+https://amtraker.com)",
-      'Amtraker-Version': "v3.21.11",
+      'User-Agent': "AmtrakerViteWorker/v3.21.12 (+https://amtraker.com)",
+      'Amtraker-Version': "v3.21.12",
     }
   });
   const infoDataRaw = await infoRes.text();
